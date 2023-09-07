b145a9830344b443f13630da791d5a10b6aa3bf8
joxavier <35542263+joxavier@users.noreply.github.com>
2023-09-07 02:57:36 -0400

    font fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
 import '../styles/globals.css'
+import localFont from 'next/font/local'
+
+const myFont = localFont({ 
+   src: '../styles/fonts/masiku/masiku.ttf',
+    variable: '--font-masiku',
+  })
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+    <main className={'${myFont.variable}'}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
 
 export default MyApp
